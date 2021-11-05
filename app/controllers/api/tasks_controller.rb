5344b24089363d5e3da4dd7f8f67ac61ae40dc02
asahi <asap1017@docomo.ne.jp>
2021-11-05 16:06:26 +0900

    create #5

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -10,5 +10,19 @@ module Api
       render json: task
     end
 
+    def create
+      task = Task.new(task_params)
+      if task.save 
+        render json: task
+      else
+        render json: task.errors, status: 422
+      end
+    end
+
+    private
+
+    def task_params
+      params[:task].permit(:title, :text)
+    end
   end
 end
