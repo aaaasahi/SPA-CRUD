ac33a607f8992f3a894d39021eee0a26acc2873c
asahi <asap1017@docomo.ne.jp>
2021-11-05 16:03:23 +0900

    show #5

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,8 +1,13 @@
 module Api
   class TasksController < ApplicationController
     def index
-      todos = Tasks.all
-      render json: todos
+      tasks = Tasks.all
+      render json: tasks
+    end
+
+    def show
+      task = Task.find(params[:id])
+      render json: task
     end
 
   end
