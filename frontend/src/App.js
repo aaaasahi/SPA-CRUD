c8faeaa2f0f94cfff2d8be338495631e2306f6ed
asahi <asap1017@docomo.ne.jp>
2021-11-12 12:02:20 +0900

    タスク一覧移動 #15

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,9 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 
 
 export const App = () => {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    getTasks();
-  }, []);
-
-  const getTasks = () => {
-    axios.get('http://localhost:8000/api/tasks')
-      .then(response => {
-        setTasks(response.data)
-        console.log(response.data);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
-
-  return (
-    <>
-      <h2 className="text-center m-5">Task List</h2>
-      <div class="card w-50 mx-auto">
-        <ul className="list-group">
-          {tasks.map((task, index) => (
-            <li className={"list-group-item"} key={index}>
-              {task.title}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
+  return 
 };
 
-export default App;
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 export const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    getTasks();
+  }, []);
+
+  const getTasks = () => {
+    axios.get('http://localhost:8000/api/tasks')
+      .then(response => {
+        setTasks(response.data)
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
   return (
-    <>一覧ページ</>
-  )
+    <>
+      <h2 className="text-center m-5">Task List</h2>
+      <div class="card w-50 mx-auto">
+        <ul className="list-group">
+          {tasks.map((task, index) => (
+            <li className={"list-group-item"} key={index}>
+              {task.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
